317404cb8cbfa61fe985de5d6cc1f8f22c0a40c3
= <alexvolkovdd@gmail.com>
2022-11-28 16:38:03 +0400

    Обновлён TimetableController

--- WebAPI/Controllers/Authenticated/Timetable/TimetableController.cs
+++ WebAPI/Controllers/Authenticated/Timetable/TimetableController.cs
@@ -18,6 +18,9 @@ namespace WebAPI.Controllers.Authenticated.Timetable
             _context = context;
         }
 
+        //TODO: Дописать
+        public List<>
+
         [HttpPost("add")]
         public IActionResult AddTimetable(AddTimetableForm form) 
         {
@@ -39,14 +42,50 @@ namespace WebAPI.Controllers.Authenticated.Timetable
             _context.Save();
             return Ok("Расписание добавлено!");
         }
-
+        ///<summary>
+        /// 
+        ///</summary>
         [HttpGet("add/{timeTableId}/items")]
         public IActionResult AddItemsInTimeTable(string timeTableId, List<LessonForm> lessons) 
         {
+            Guid id = Guid.Parse(timeTableId);
 
+            TimeTableModel? timeTable = _context.GetTimeTable()
+                .Where(g => g.Id == id)
+                .First();
 
+            if (timeTable == null)
+                return BadRequest("Расписание не создано!");
+
+            lessons.ForEach(item => _context.GetTimeTableLessonModel().Add(item));
+
+            _context.Save();
 
             return Ok("Пары добавлены в расписание!");
+        }
+
+        [HttpGet("edit/{timeTableId}")]
+        public IActionResult EditTimeTable(string id, List<LessonForm> data) 
+        {
+            Guid id = Guid.Parse(id);
+
+            var day = _context
+                .GetTimeTableLessonModel()
+                .Where(i => i.TimetableId == id)
+                .ToList();
+            
+            if (!day)
+                return BadRequest("Расписание не найдено!");
+
+            for (int i = 0; i < day.Lenght(); i++) {
+                day[i].DisciplineId = data[i].DisciplineId;
+                day[i].EducatorId = data[i].EducatorId;
+                day[i].CabinetNumber = data[i].CabinetNumber;
+            }
+
+            _context.Save();
+
+            return Ok("Изменения успешно внесены!");
         }
 
     }
